ff6f0d3a0d684707684075d8af9bd8d8862b8e16
Alexandr <smolnikov.456@mail.ru>
2023-04-18 23:16:36 +0500

    update app controller
    added user collection clearing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { PostSchema } from './product-module/product-domain/post/post.entity';
 import { Model } from 'mongoose';
 import { BlogSchema } from './product-module/product-domain/blog/blog.entity';
+import { UserSchema } from './auth-module/auth-domain/user/user.entity';
 
 @Controller('testing')
 export class AppController {
   constructor(
     @InjectModel(PostSchema.name) private PostModel: Model<PostSchema>,
     @InjectModel(BlogSchema.name) private BlogModel: Model<BlogSchema>,
+    @InjectModel(UserSchema.name) private UserModel: Model<UserSchema>,
   ) {}
   @Delete('all-data')
   @HttpCode(HttpStatus.NO_CONTENT)
@@ -16,6 +18,7 @@ export class AppController {
     await Promise.all([
       this.PostModel.deleteMany(),
       this.BlogModel.deleteMany(),
+      this.UserModel.deleteMany(),
     ]);
   }
 }
