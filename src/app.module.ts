7ad9d7668513033b7c877aaa3d6c79151ed7eed7
Alexandr <smolnikov.456@mail.ru>
2023-04-19 10:15:55 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,18 +11,24 @@ import {
   BlogSchema,
 } from './product-module/product-domain/blog/blog.entity';
 import { AuthModule } from './auth-module/auth.module';
+import { AppController } from './app.controller';
+import {
+  userSchema,
+  UserSchema,
+} from './auth-module/auth-domain/user/user.entity';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(envVariables.MONGO_LOCAL),
+    MongooseModule.forRoot(envVariables.MONGO_URL),
     ProductModule,
     AuthModule,
     MongooseModule.forFeature([
       { name: PostSchema.name, schema: postSchema },
       { name: BlogSchema.name, schema: blogSchema },
+      { name: UserSchema.name, schema: userSchema },
     ]),
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
