78c160b862a39eeb9f37abef02b8d9e1bc42bf85
Alexandr <smolnikov.456@mail.ru>
2023-06-06 08:55:24 +0500

    blogger api additional fixes

--- src/blogger-api/blog/api/blog-blogger.controller.ts
+++ src/blogger-api/blog/api/blog-blogger.controller.ts
@@ -42,6 +42,7 @@ export class BlogBloggerController {
   ) {}
 
   @Post()
+  @UseGuards(AccessTokenGuard)
   @HttpCode(HttpStatus.CREATED)
   async createBlog(
     @Body() blogCreateDTO: BlogBloggerApiCreateUpdateDTO,
@@ -61,6 +62,7 @@ export class BlogBloggerController {
   }
 
   @Post(':blogId/posts')
+  @UseGuards(AccessTokenGuard)
   @HttpCode(HttpStatus.CREATED)
   async createPostForSpecificBlog(
     @Param('blogId') blogId: string,
