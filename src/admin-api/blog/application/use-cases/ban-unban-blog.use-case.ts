1d20be91454b65cc4b9563ddd423b7440d6e0994
Alexandr <smolnikov.456@mail.ru>
2023-07-12 04:26:18 +0500

    admin api ban/unban blog use case moved to postgres

--- src/admin-api/blog/application/use-cases/ban-unban-blog.use-case.ts
+++ src/admin-api/blog/application/use-cases/ban-unban-blog.use-case.ts
@@ -6,6 +6,8 @@ import { Model } from 'mongoose';
 import { PostSchema } from '../../../../../libs/db/mongoose/schemes/post.entity';
 import { CommentSchema } from '../../../../../libs/db/mongoose/schemes/comment.entity';
 import { LikeSchema } from '../../../../../libs/db/mongoose/schemes/like.entity';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 export class BanUnbanBlogCommand {
   constructor(
@@ -22,6 +24,7 @@ export class BanUnbanBlogUseCase
     @InjectModel(PostSchema.name) private PostModel: Model<PostSchema>,
     @InjectModel(CommentSchema.name) private CommentModel: Model<CommentSchema>,
     @InjectModel(LikeSchema.name) private LikeModel: Model<LikeSchema>,
+    @InjectDataSource() private readonly dataSource: DataSource,
   ) {}
 
   async execute({
@@ -35,22 +38,66 @@ export class BanUnbanBlogUseCase
   }
 
   async banBlog(blogId: string): Promise<void> {
-    await this.BlogModel.updateMany(
-      { id: blogId },
-      { hidden: true, isBanned: true, banDate: new Date().toISOString() },
-    );
-    await this.PostModel.updateMany({ blogId }, { hidden: true });
-    await this.CommentModel.updateMany({ blogId }, { hidden: true });
-    await this.LikeModel.updateMany({ blogId }, { hidden: true });
+    const hideBlogs = async (): Promise<void> => {
+      await this.dataSource.query(
+        `
+      UPDATE public.blogs
+      SET "hidden" = true, "is_banned" = true, "ban_date" = now()
+      WHERE id = $1
+      `,
+        [blogId],
+      );
+    };
+    await hideBlogs();
+    const hidePosts = async (): Promise<void> => {
+      await this.dataSource.query(
+        `
+      UPDATE public.posts
+      SET "hidden" = true
+      WHERE "blog_id" = $1
+      `,
+        [blogId],
+      );
+    };
+    await hidePosts();
+    // await this.BlogModel.updateMany(
+    //   { id: blogId },
+    //   { hidden: true, isBanned: true, banDate: new Date().toISOString() },
+    // );
+    // await this.PostModel.updateMany({ blogId }, { hidden: true });
+    // await this.CommentModel.updateMany({ blogId }, { hidden: true });
+    // await this.LikeModel.updateMany({ blogId }, { hidden: true });
   }
 
   async unbanBlog(blogId: string): Promise<void> {
-    await this.BlogModel.updateMany(
-      { id: blogId },
-      { hidden: false, isBanned: false, banDate: null },
-    );
-    await this.PostModel.updateMany({ blogId }, { hidden: false });
-    await this.CommentModel.updateMany({ blogId }, { hidden: false });
-    await this.LikeModel.updateMany({ blogId }, { hidden: false });
+    const revealBlogs = async (): Promise<void> => {
+      await this.dataSource.query(
+        `
+      UPDATE public.blogs
+      SET "hidden" = false, "is_banned" = false, "ban_date" = null
+      WHERE id = $1
+      `,
+        [blogId],
+      );
+    };
+    await revealBlogs();
+    const revealPosts = async (): Promise<void> => {
+      await this.dataSource.query(
+        `
+      UPDATE public.posts
+      SET "hidden" = false
+      WHERE "blog_id" = $1
+      `,
+        [blogId],
+      );
+    };
+    await revealPosts();
+    // await this.BlogModel.updateMany(
+    //   { id: blogId },
+    //   { hidden: false, isBanned: false, banDate: null },
+    // );
+    // await this.PostModel.updateMany({ blogId }, { hidden: false });
+    // await this.CommentModel.updateMany({ blogId }, { hidden: false });
+    // await this.LikeModel.updateMany({ blogId }, { hidden: false });
   }
 }
