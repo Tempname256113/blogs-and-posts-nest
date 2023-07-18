7908c5d9dbddcaa02cbd188271ce3124ba08e9f5
Alexandr <smolnikov.456@mail.ru>
2023-07-19 02:30:27 +0500

    admin api ban/unban user use case fix

--- src/admin-api/user/application/use-cases/ban-unban-user.use-case.ts
+++ src/admin-api/user/application/use-cases/ban-unban-user.use-case.ts
@@ -2,6 +2,8 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UserBanUnbanDTO } from '../../api/models/user-api.dto';
 import { SecurityRepositorySQL } from '../../../../public-api/security/infrastructure/repositories/security.repository-sql';
 import { UserRepositorySQL } from '../../infrastructure/repositories/user.repository-sql';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 export class BanUnbanUserCommand {
   constructor(
@@ -19,6 +21,7 @@ export class BanUnbanUserUseCase
   constructor(
     private readonly securityRepositorySQL: SecurityRepositorySQL,
     private readonly usersRepositorySQL: UserRepositorySQL,
+    @InjectDataSource() private readonly dataSource: DataSource,
   ) {}
 
   async execute({
@@ -44,6 +47,46 @@ export class BanUnbanUserUseCase
       banReason,
       userId,
     });
+    const rawBlogsId: { id: number }[] = await this.dataSource.query(
+      `
+    SELECT b."id"
+    FROM public.blogs b
+    WHERE b."blogger_id" = $1
+    `,
+      [userId],
+    );
+    const blogsId: string[] = rawBlogsId.map((rawBlog) => {
+      return String(rawBlog.id);
+    });
+    const rawPostsId: { id: number }[] = await this.dataSource.query(`
+    UPDATE public.posts
+    SET "hidden" = true
+    WHERE "blog_id" = ANY (ARRAY ${blogsId})
+    `);
+    const postsId: string[] = rawPostsId.map((rawPost) => {
+      return String(rawPost.id);
+    });
+    await this.dataSource.query(`
+    UPDATE public.comments
+    SET "hidden" = true
+    WHERE "post_id" = ANY (ARRAY ${postsId})
+    `);
+    await this.dataSource.query(
+      `
+    UPDATE public.comments_likes
+    SET "hidden" = true
+    WHERE "user_id" = $1
+    `,
+      [userId],
+    );
+    await this.dataSource.query(
+      `
+    UPDATE public.posts_likes
+    SET "hidden" = true
+    WHERE "user_id" = $1
+    `,
+      [userId],
+    );
     // await this.PostModel.updateMany({ bloggerId: userId }, { hidden: true });
     // await this.CommentModel.updateMany({ userId }, { hidden: true });
     // await this.LikeModel.updateMany({ userId }, { hidden: true });
@@ -54,6 +97,46 @@ export class BanUnbanUserUseCase
       isBanned: false,
       userId,
     });
+    const rawBlogsId: { id: number }[] = await this.dataSource.query(
+      `
+    SELECT b."id"
+    FROM public.blogs b
+    WHERE b."blogger_id" = $1
+    `,
+      [userId],
+    );
+    const blogsId: string[] = rawBlogsId.map((rawBlog) => {
+      return String(rawBlog.id);
+    });
+    const rawPostsId: { id: number }[] = await this.dataSource.query(`
+    UPDATE public.posts
+    SET "hidden" = false
+    WHERE "blog_id" = ANY (ARRAY ${blogsId})
+    `);
+    const postsId: string[] = rawPostsId.map((rawPost) => {
+      return String(rawPost.id);
+    });
+    await this.dataSource.query(`
+    UPDATE public.comments
+    SET "hidden" = false
+    WHERE "post_id" = ANY (ARRAY ${postsId})
+    `);
+    await this.dataSource.query(
+      `
+    UPDATE public.comments_likes
+    SET "hidden" = false
+    WHERE "user_id" = $1
+    `,
+      [userId],
+    );
+    await this.dataSource.query(
+      `
+    UPDATE public.posts_likes
+    SET "hidden" = false
+    WHERE "user_id" = $1
+    `,
+      [userId],
+    );
     // await this.PostModel.updateMany({ bloggerId: userId }, { hidden: false });
     // await this.CommentModel.updateMany({ userId }, { hidden: false });
     // await this.LikeModel.updateMany({ userId }, { hidden: false });
