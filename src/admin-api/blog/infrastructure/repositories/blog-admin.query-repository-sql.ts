7b6d2ca801df3f78a7756eca4ca48a1274f109ed
Alexandr <smolnikov.456@mail.ru>
2023-08-11 09:44:35 +0500

    admin api get all blogs use case moved to typeorm

--- src/admin-api/blog/infrastructure/repositories/blog-admin.query-repository-sql.ts
+++ src/admin-api/blog/infrastructure/repositories/blog-admin.query-repository-sql.ts
@@ -1,75 +1,125 @@
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository, SelectQueryBuilder } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { BlogAdminApiPaginationQueryDTO } from '../../api/models/blog-admin-api.query-dto';
 import {
   AdminApiBlogViewModel,
   AdminApiBlogsPaginationModel,
 } from '../../api/models/blog-admin-api.models';
+import { BlogSQLEntity } from '../../../../../libs/db/typeorm-sql/entities/blog-sql.entity';
+import { UserSQLEntity } from '../../../../../libs/db/typeorm-sql/entities/users/user-sql.entity';
 
 @Injectable()
 export class AdminBlogQueryRepositorySQL {
-  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
+  constructor(
+    @InjectDataSource() private readonly dataSource: DataSource,
+    @InjectRepository(BlogSQLEntity)
+    private readonly blogEntity: Repository<BlogSQLEntity>,
+  ) {}
 
   async getBlogsWithPagination(
     paginationQuery: BlogAdminApiPaginationQueryDTO,
   ): Promise<AdminApiBlogsPaginationModel> {
-    let filter = '';
-    if (paginationQuery.searchNameTerm) {
-      filter = `WHERE b."name" ILIKE '%${paginationQuery.searchNameTerm}%'`;
-    }
-    let orderBy: string;
-    const getCorrectOrderBy = (): void => {
+    const getFilter = (): {
+      query: string;
+      additional: { [key: string]: string };
+    } => {
+      let filter = '';
+      const additional: { [key: string]: string } = {};
+      if (paginationQuery.searchNameTerm) {
+        filter = `WHERE b.name ILIKE :blogName`;
+        additional.blogName = `%${paginationQuery.searchNameTerm}%;`;
+      }
+      return {
+        query: filter,
+        additional,
+      };
+    };
+    const getCorrectOrderBy = (): {
+      query: string;
+      sortDirection: 'ASC' | 'DESC';
+    } => {
+      const correctSortDirection: 'ASC' | 'DESC' =
+        paginationQuery.sortDirection === 'asc' ? 'ASC' : 'DESC';
       switch (paginationQuery.sortBy) {
         case 'createdAt':
-          orderBy = 'b."created_at"';
-          break;
+          return {
+            query: 'b.createdAt',
+            sortDirection: correctSortDirection,
+          };
         case 'name':
-          orderBy = 'b."name"';
-          break;
+          return {
+            query: 'b.name',
+            sortDirection: correctSortDirection,
+          };
         case 'description':
-          orderBy = 'b."description';
-          break;
+          return {
+            query: 'b.description',
+            sortDirection: correctSortDirection,
+          };
       }
     };
-    getCorrectOrderBy();
-    const blogsCount: [{ count: number }] = await this.dataSource.query(`
-    SELECT COUNT(*)
-    FROM public.blogs b
-    ${filter}
-    `);
-    const totalBlogsCount: number = blogsCount[0].count;
+    const filter: ReturnType<typeof getFilter> = getFilter();
+    const orderBy: ReturnType<typeof getCorrectOrderBy> = getCorrectOrderBy();
+    const getTotalBlogsCount = async (): Promise<number> => {
+      const queryBuilder: SelectQueryBuilder<BlogSQLEntity> =
+        await this.dataSource.createQueryBuilder(BlogSQLEntity, 'b');
+      return queryBuilder.where(filter.query, filter.additional).getCount();
+    };
+    const totalBlogsCount: number = await getTotalBlogsCount();
     const howMuchToSkip: number =
       paginationQuery.pageSize * (paginationQuery.pageNumber - 1);
     const pagesCount: number = Math.ceil(
       totalBlogsCount / paginationQuery.pageSize,
     );
-    const foundedBlogs: any[] = await this.dataSource.query(`
-        SELECT b."id" as "blog_id", b."name", b."description", b."website_url", b."created_at",
-         b."is_membership", b."is_banned", b."ban_date",
-         u."id" as "user_id", u."login" as "user_login"
-        FROM public.blogs b
-        LEFT JOIN public.users u on b."blogger_id" = u."id"
-        ${filter}
-        ORDER BY ${orderBy} ${paginationQuery.sortDirection.toUpperCase()}
-        LIMIT ${paginationQuery.pageSize} OFFSET ${howMuchToSkip}
-        `);
+    const queryBuilder: SelectQueryBuilder<BlogSQLEntity> =
+      await this.dataSource.createQueryBuilder(BlogSQLEntity, 'b');
+    const foundedBlogs: {
+      b_id: number;
+      b_name: string;
+      b_description: string;
+      b_websiteUrl: string;
+      b_createdAt: string;
+      b_isMembership: boolean;
+      b_isBanned: boolean;
+      b_banDate: string | null;
+      u_id: number;
+      u_login: string;
+    }[] = await queryBuilder
+      .select([
+        'b.id',
+        'b.name',
+        'b.description',
+        'b.websiteUrl',
+        'b.createdAt',
+        'b.isMembership',
+        'b.isBanned',
+        'b.banDate',
+        'u.id',
+        'u.login',
+      ])
+      .leftJoin(UserSQLEntity, 'u', 'b.bloggerId = u.id')
+      .where(filter.query, filter.additional)
+      .orderBy(orderBy.query, orderBy.sortDirection)
+      .limit(paginationQuery.pageSize)
+      .offset(howMuchToSkip)
+      .getRawMany();
     const mappedBlogs: AdminApiBlogViewModel[] = foundedBlogs.map(
       (blogFromDB) => {
         const mappedBlog: AdminApiBlogViewModel = {
-          id: String(blogFromDB.blog_id),
-          name: blogFromDB.name,
-          description: blogFromDB.description,
-          websiteUrl: blogFromDB.website_url,
-          createdAt: blogFromDB.created_at,
-          isMembership: blogFromDB.is_membership,
+          id: String(blogFromDB.b_id),
+          name: blogFromDB.b_name,
+          description: blogFromDB.b_description,
+          websiteUrl: blogFromDB.b_websiteUrl,
+          createdAt: blogFromDB.b_createdAt,
+          isMembership: blogFromDB.b_isMembership,
           blogOwnerInfo: {
-            userId: blogFromDB.user_id ? String(blogFromDB.user_id) : null,
-            userLogin: blogFromDB.user_login,
+            userId: blogFromDB.u_id ? String(blogFromDB.u_id) : null,
+            userLogin: blogFromDB.u_login,
           },
           banInfo: {
-            isBanned: blogFromDB.is_banned,
-            banDate: blogFromDB.ban_date,
+            isBanned: blogFromDB.b_isBanned,
+            banDate: blogFromDB.b_banDate,
           },
         };
         return mappedBlog;
